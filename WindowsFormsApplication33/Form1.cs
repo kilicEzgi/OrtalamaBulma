417dfd76493b2cf9717168f4e1291f98f8fcdd70
kilicEzgi <99246213+kilicEzgi@users.noreply.github.com>
2022-02-18 08:56:51 +0300

    Add files via upload

---
+++ WindowsFormsApplication33/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication33
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double toplam = 0;
+            int sayac = 0;
+            double ortalama;
+            while (sayac< listBox1.Items.Count)
+            {
+                 toplam=toplam + Convert.ToDouble(listBox1.Items[sayac]);
+                 sayac++;
+            }
+            ortalama = toplam / sayac;
+            label1.Text = "Ortalama" + ortalama.ToString();
+        }
+    }
+}
